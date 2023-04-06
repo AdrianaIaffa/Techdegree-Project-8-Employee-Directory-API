1957da17d4184165c39a1b1e9f998c8beb3af958
AdrianaIaffa <adrianaiaffa@hotmail.com>
2023-04-06 18:09:36 +0100

    add overlay class move under main tags

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let employees = [];
 const urlAPI =`https://randomuser.me/api/?results=12&inc=name,picture,email, location, phone, dob &noinfo &nat=US`;
 const gridContainer = document.querySelector(".grid-container");
 const overlay = document.querySelector(".overlay");
-const modalContainer = document.querySelector(".modal-content");
+const modalContainer = document.querySelector(".modal");
 const modalClose = document.querySelector(".modal-close");
 
 //Fetch data from API
